c296ace16bc603993c53b546ce99185ae95de615
Thanakrit.J <Thanakrit.J@roki-thailand.com>
2019-06-06 16:55:56 +0700

    update access class

---
+++ TT1995APIs/Controllers/AccountController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TT1995APIs.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult CheckLogin()
+        {
+            List<CustomerModels> ul = new List<CustomerModels>();
+            using (SqlConnection con = ConnectDatabase(Properties.Settings.Default.IPAddress, Properties.Settings.Default.Username, Properties.Settings.Default.Password))
+            {
+                string _SQL = "SELECT * FROM [TT1995_CheckList].[dbo].[customer]";
+                using (SqlCommand cmd = new SqlCommand(_SQL, con))
+                {
+                    DataTable _Dt = new DataTable();
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    da.Fill(_Dt);
+                    da.Dispose();
+                    foreach (DataRow _Item in _Dt.Rows)
+                    {
+                        CustomerModels CM = new CustomerModels();
+                        CM.customer_id = Int32.Parse(_Item["cus_id"].ToString());
+                        CM.customer_name = _Item["cus_name"].ToString();
+                        ul.Add(CM);
+                    }
+                }
+                con.Close();
+            }
+            return Json(ul, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ TT1995APIs/Models/Account/AccountModels.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TT1995APIs.Models.Account
+{
+    public class AccountStatusModels
+    {
+        public int user_id { get; set; }
+        public string username { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string login_status { get; set; }
+    }
+
+    public class PermissionAccount
+    {
+        public int application_id { get; set; }
+        public string application_name { get; set; }
+        public int access_status { get; set; }
+    }
+}
